772cdf922a0e01f5dc184fb5afc36f65f7643049
maxmasse <maxmasse03@gmail.com>
2023-10-03 11:16:36 -0400

    rajout du footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import Header from "./assets/Components/Header"
+import Header from "./assets/Components/Header";
+import Footer from "./assets/Components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col" >
+    <div className="min-h-screen flex flex-col">
+      <Header />
 
-      <Header /> 
-
-      <main className="flex flex-wrap justify center items-center flex-grow bg-gray-100 px-10 md:flex-nowrap "> 
-      
-      
-      {/* <Leftside />  */}
-      {/* <Visualisation />  */}
-
-      
+      <main className="flex flex-wrap justify center items-center flex-grow bg-gray-100 px-10 md:flex-nowrap ">
+        {/* <Leftside />  */}
+        {/* <Visualisation />  */}
       </main>
 
-      {/* <Footer/> */}
-
+      <Footer/>
     </div>
   );
 }
---
+++ src/assets/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="p-2 border-t-2 border-gray-200">
+        <p> Boxy - All right reserved &copy;</p>
+      </footer>
+    </>
+  );
+}
