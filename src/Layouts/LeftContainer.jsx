26f1d2e21ccf010857912607b2aca8619305326c
maxmasse <maxmasse03@gmail.com>
2023-10-03 11:46:06 -0400

    création de tabList

--- src/Layouts/LeftContainer.jsx
+++ src/Layouts/LeftContainer.jsx
@@ -1,12 +1,19 @@
-import {useState} from "react"
-import ShadowList from "./ShadowList/ShadowList"
-import BoxPanel from "./BoxPanel/BoxPanel"
+import { useState } from "react";
+import ShadowList from "./ShadowList/ShadowList";
+import BoxPanel from "./BoxPanel/BoxPanel";
 
-
-import React from 'react'
+import React from "react";
 
 export default function LeftContainer() {
+  const [tab, setTabs] = useState(0);
+
+  const tabList = [
+    { name: "shadow", component:<ShadowList/> },
+    { name: "boxPanel", component:<BoxPanel/> },
+
+];
+
   return (
-    <div>LeftContainer</div>
-  )
+    <div className="relative mt-20 w-[600px] h-[550px] border rounded-b rounded-tr border-gray-300 shadow-xl bg-gray-50 md:mt-0"></div>
+  );
 }
