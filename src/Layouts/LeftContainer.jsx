69acbe03bcbd8549a8e5bef4d7f089d84c4bf92c
maxmasse <maxmasse03@gmail.com>
2023-10-03 14:19:55 -0400

    ajustement composante boxpanel et shadowList

--- src/Layouts/LeftContainer.jsx
+++ src/Layouts/LeftContainer.jsx
@@ -28,13 +28,11 @@ export default function LeftContainer() {
           );
         })}
       </div>
-{/* affichage des élément des composante boxPanel et ShadowList */}
+      {/* affichage des élément des composante boxPanel et ShadowList */}
 
-<div className="flex-shrink-0 h-full max-h-[700px] overflow-auto">
-  
-  {tabList[tab].component}
-   </div>
-
+      <div className="flex-shrink-0 h-full max-h-[700px] overflow-auto">
+        {tabList[tab].component}
+      </div>
     </div>
   );
 }
