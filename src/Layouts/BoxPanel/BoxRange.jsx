61661266860030a6d265a6ea5c7a90de8ecd1af9
maxmasse <maxmasse03@gmail.com>
2023-10-04 13:00:46 -0400

    destructuring input data

--- src/Layouts/BoxPanel/BoxRange.jsx
+++ src/Layouts/BoxPanel/BoxRange.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export default function BoxRange() {
+export default function BoxRange({ inputData }) {
   return (
-    <div>BoxRange</div>
-  )
+    <div className="my-8">
+      <div className="flex justify-between">
+        {/* Destructuring */}
+        <p>{inputData.name}</p>
+
+        <div className="flex items-baseline mb-2">
+          <input
+            className="w-14 h-8 mr-2 border-gray-200 text-center"
+            type="number"
+          />
+          <p>px</p>
+        </div>
+      </div>
+    </div>
+  );
 }
