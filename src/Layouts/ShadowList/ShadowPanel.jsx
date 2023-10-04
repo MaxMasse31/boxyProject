ad0c0217de8551bf51792ede899a0c5382015b18
maxmasse <maxmasse03@gmail.com>
2023-10-04 19:41:43 -0400

    rajout du bouton +chevron

--- src/Layouts/ShadowList/ShadowPanel.jsx
+++ src/Layouts/ShadowList/ShadowPanel.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Chevron from "../../assets/chevron.svg";
 
-export default function ShadowPanel() {
+export default function ShadowPanel({ panelNumber, shadow }) {
+  const [toggleShadow, setToggleShadow] = useState(true);
+
   return (
     <>
-      <li>Shadow</li>
+      <li className="bg-gray-50 border-b border-gray-300">
+        <button onClick={() => setToggleShadow(!toggleShadow)}>
+          <span>Shadow {panelNumber} </span>
+          <img
+            style={{
+              transform: `${toggleShadow ? "rotate(90deg)" : "rotate(0deg)"}`,
+            }}
+            className="font-bold w-5"
+            src={Chevron}
+            alt="Chevron"
+          />
+        </button>
+        {toggleShadow && (
+          <>
+            <div className="flex item-end px-6 py-4">
+              {/* Checkbox */}
+
+              <button className=" ml-auto text-sm bg-red-600 text-white hover:bg-red-700 py-1 px-3 rounded">
+                Remove
+              </button>
+            </div>
+          </>
+        )}
+      </li>
     </>
   );
 }
