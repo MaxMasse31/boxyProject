d21ad38e35f5288cec304e497e2d5ca4a3bce82b
maxmasse <maxmasse03@gmail.com>
2023-10-04 15:37:10 -0400

    ajout de l'input color + config avec le dispatch updateBoxeValue

--- src/Layouts/BoxPanel/BoxColorPicker.jsx
+++ src/Layouts/BoxPanel/BoxColorPicker.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { updateBoxeValue } from "../../features/boxProperties";
 
-export default function BoxColorPicker() {
+export default function BoxColorPicker({ inputData }) {
+  const dispatch = useDispatch();
+
+  function handleInputs(e) {
+    dispatch(
+      updateBoxeValue({
+        inputNumber: inputData.inputNumber,
+        value: e.target.value,
+      })
+    );
+  }
+
   return (
-    <div>BoxColorPicker</div>
-  )
+    <div className="mt-3">
+      {/* destructuring avec props InputData */}
+      <p>{inputData.name} </p>
+
+      <div className="flex mt-2">
+        <input
+          value={inputData.value}
+          onChange={handleInputs}
+          className="flex-grow border py-1 px-2 focus:outline-1 outline-grey-400"
+          type="text"
+        />
+
+        <input
+          className="cursor-pointer h-[40px]"
+          type="color"
+          value={inputData.value}
+          onChange={handleInputs}
+        />
+      </div>
+    </div>
+  );
 }
