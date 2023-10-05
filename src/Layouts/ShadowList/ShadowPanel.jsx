77513525268c96a5ec797686a7df35128c4e2ee4
maxmasse <maxmasse03@gmail.com>
2023-10-04 22:10:08 -0400

    ajout d'animation sur shadowPanel

--- src/Layouts/ShadowList/ShadowPanel.jsx
+++ src/Layouts/ShadowList/ShadowPanel.jsx
@@ -1,19 +1,23 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Chevron from "../../assets/chevron.svg";
+import "./animation.css";
+import { useSpring, animated } from "react-spring";
 
 export default function ShadowPanel({ panelNumber, shadow }) {
   const [toggleShadow, setToggleShadow] = useState(true);
+  const rotateProps = useSpring({
+    transform: toggleShadow ? "rotate(90deg)" : "rotate(0deg)",
+  });
 
   return (
     <>
-      <li className="bg-gray-50 border-b border-gray-300">
+      <li
+        className= "bg-gray-50 border-b border-gray-300 animation">
+      
         <button onClick={() => setToggleShadow(!toggleShadow)}>
           <span>Shadow {panelNumber} </span>
-          <img
-            style={{
-              transform: `${toggleShadow ? "rotate(90deg)" : "rotate(0deg)"}`,
-            }}
+          <animated.img
+            style={rotateProps}
             className="font-bold w-5"
             src={Chevron}
             alt="Chevron"
@@ -23,8 +27,7 @@ export default function ShadowPanel({ panelNumber, shadow }) {
           <>
             <div className="flex item-end px-6 py-4">
               {/* Checkbox */}
-
-              <button className=" ml-auto text-sm bg-red-600 text-white hover:bg-red-700 py-1 px-3 rounded">
+              <button className="ml-auto text-sm bg-red-600 text-white hover:bg-red-700 py-1 px-3 rounded">
                 Remove
               </button>
             </div>
