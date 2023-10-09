f2a7b303f0c4bab2b311b74ab0a52830520e3101
maxmasse <maxmasse03@gmail.com>
2023-10-09 17:04:19 -0400

    créer le contenue de la modale

--- src/Layouts/Visualization/Modal/ModalBtn.jsx
+++ src/Layouts/Visualization/Modal/ModalBtn.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { createPortal } from "react-dom";
-import ModalResult from "./ModalResult"
+import ModalResult from "./ModalResult";
 
 import React from "react";
 
@@ -9,19 +9,19 @@ export default function ModalBtn() {
 
   return (
     <>
-      <button 
-      onClick={()=>setShowModal()}
-      className="relative z-0 mx-auto mt-2 py-1 px-3 text-sm rounded bg-blue-600 text-white hover:bg-blue-700">
+      <button
+        onClick={() => setShowModal(true)}
+        className="relative z-0 mx-auto mt-2 py-1 px-3 text-sm rounded bg-blue-600 text-white hover:bg-blue-700"
+      >
         Get the code
       </button>
 
-      {showModal && createPortal(<ModalResult closeModal={()=>setShowModal(!showModal)}/>,document.body
-      
-      
-      
-      
-      
-      )}
+      {/* Activer la modale */}
+      {showModal &&
+        createPortal(
+          <ModalResult closeModal={() => setShowModal(!showModal)} />,
+          document.body
+        )}
     </>
   );
 }
--- src/Layouts/Visualization/Modal/ModalResult.jsx
+++ src/Layouts/Visualization/Modal/ModalResult.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
+import getBoxShadowValue from "../../../utils/getBoxShadowValue";
+import { useSelector } from "react-redux";
 
-export default function ModalResult() {
+export default function ModalResult({ closeModal }) {
+  const shadowValues = useSelector((state) => state.shadows);
   return (
-    <div>ModalResult</div>
-  )
+    <div
+      onClick={closeModal}
+      className="fixed z-10 inset-0 flex items-center justify-center bg-gray-600/75"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="max-w-[400px] rounded p-7 bg-gray-50 mb-[10vh]"
+      >
+        <div className="flex items-end mb-5  ">
+          <p className="font-semibold mr-5"> Here is your code</p>
+
+          <button className="ml-10 mr-0 text-sm bg-blue-600 text-white hover:bg-blue-700 py-1 px-3 rounded  ">
+            copy
+          </button>
+
+          <button
+            onClick={closeModal}
+            className="ml-auto mr-2 text-sm bg-red-600  text-white hover:bg-red-800 py-1 px-3 rounded  "
+          >
+            close
+          </button>
+        </div>
+        <p className="rounded bg-gray-100 p-5">
+          <span className="font-semibold">
+            box-shadow: {getBoxShadowValue(shadowValues)}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 }
